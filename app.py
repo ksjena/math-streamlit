d3cca925a08b61e14db4da347c42873ad8d9ba2b
ksjena <90602493+ksjena@users.noreply.github.com>
2023-12-13 07:39:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st
 
-n1 = st.number_input("Enter a number")
-st.wirte(n1)
+st.write("Simple illustration of finding max of three numbers")
+
+st.header("User Inputs numbers")
+
+def user_input_number():
+  number1 = st.number_input("number_1")
+  number2 = st.number_input("number_2")
+  number3 = st.number_input("number_3")
+
+  if number1 > number2 and number1 > number3:
+    result = number1
+  elif number2 > number1 and number2 > number3:
+    result = number2
+  elif number3 > number1 and number3 > number2:
+    result = number3
+  elif number1 == number2 and number2 == number3:
+    result = "All number are equal"
+  
+  return result
+
+st.subheader('Here is the max of 3 number')
+st.wirte(result)
